d2a81e7122d87332a1e1536deaf5c8ed4f55569f
Alexander Voelz <alexander.voelz@de.ibm.com>
2020-06-25 14:37:45 +0200

    cleanup

--- PointsUI/Views/ContentView.swift
+++ PointsUI/Views/ContentView.swift
@@ -12,49 +12,28 @@ import SwiftUI
 struct ContentView: View {
     
     @ObservedObject var settings = GameSettings()
-    
-    var players : Players {
-        return settings.players
-    }
-    
-    @State private var isPresented = false
         
-    func undo() {
-        settings.history.undo()
-    }
+    // MARK: Change Appearance
+    @State var navigationBarIsHidden = true
+    @State private var settingsEditorIsShown = false
     
     var body: some View {
-        ZStack {
-            TabView {
-
-                BoardUI()
-                    .tabItem({ Image(systemName: "rectangle.grid.2x2")})
-                    .tag(0)
-                    .onTapGesture {
-                        settings.resetTimer()
-                    }
-
-                ScoreTableView(viewMode: .diff)
-                    .tabItem({ Image(systemName: "table") })
-                    .tag(1)
-
-                ScoreTableView(viewMode: .total)
-                    .tabItem({ Image(systemName: "table.fill") })
-                    .tag(2)
+        NavigationView {
+            ZStack {
+                Color.lightMidnight
+                    .edgesIgnoringSafeArea(.all)
+                
+                GameBoardView()
             }
-            .background(Color.boardbgColor)
-        }
-        .environmentObject(settings)
-        .navigationBarTitle(GameSettings.name)
-        .navigationBarHidden(true)
-        .onTapGesture(count: 2) {
-            navigationBarIsHidden.toggle()
+            .environmentObject(settings)
+            .navigationBarTitle(GameSettings.name)
+            .navigationBarHidden(true)
+            .onTapGesture(count: 2) {
+                navigationBarIsHidden.toggle()
+            }
         }
     }
     
-    
-    // MARK: Local variables
-    @State var navigationBarIsHidden = true
 }
     
 
@@ -62,5 +41,29 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct GameBoardView: View {
+    @EnvironmentObject var settings: GameSettings
+    
+    var body: some View {
+        TabView {
+            
+            BoardUI()
+                .tabItem({ Image(systemName: "rectangle.grid.2x2")})
+                .tag(0)
+                .onTapGesture {
+                    settings.resetTimer()
+                }
+            
+            ScoreTableView(viewMode: .diff)
+                .tabItem({ Image(systemName: "table") })
+                .tag(1)
+            
+            ScoreTableView(viewMode: .total)
+                .tabItem({ Image(systemName: "table.fill") })
+                .tag(2)
+        }
     }
 }
