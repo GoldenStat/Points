f560e722e69d512771064a1cd6d402b43c45e259
Alexander Voelz <alexander.voelz@de.ibm.com>
2020-06-25 14:41:13 +0200

    more cleanup

--- PointsUI/Views/BoardUI.swift
+++ PointsUI/Views/BoardUI.swift
@@ -8,34 +8,28 @@
 
 import SwiftUI
 
-
-
 /// the whole board, all player's points are seen here
 struct BoardUI: View {
     
     @EnvironmentObject var settings: GameSettings
-    @State private var games : Int = 0
     
-    var history : History { settings.history }
-    var players: Players { settings.players }
-    
-    static let maxGames = GlobalSettings.maxGames
-    static let columns = 2
-    
-    var numberOfPlayers : Int { players.items.count }
+    var numberOfPlayers : Int { settings.players.items.count }
     
     var body: some View {
-        FlowStack(columns: Self.columns, numItems: numberOfPlayers, alignment: .center) {
-            index, colWidth in
-            PlayerView(player: self.players.items[index])
+        FlowStack(columns: columns,
+                  numItems: numberOfPlayers,
+                  alignment: .center) { index, colWidth in
+            PlayerView(player: settings.players.items[index])
         }
     }
+    
+    // MARK: local variables
+    private var maxGames : Int { GlobalSettings.maxGames }
+    private let columns = 2
+
 }
 
 struct BoardUI_Previews: PreviewProvider {
-    
-    static var defaultPlayers = Players(names: GlobalSettings.playerNames)
-
 	static var previews: some View {
         BoardUI()
     }
