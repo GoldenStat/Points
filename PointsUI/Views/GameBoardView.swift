3a91028b56134419306fd6bca85f9183b48fa2fa
Alexander Voelz <alexander.voelz@de.ibm.com>
2020-07-02 16:28:43 +0200

    added some animation to view changes

--- PointsUI/Views/GameBoardView.swift
+++ PointsUI/Views/GameBoardView.swift
@@ -8,7 +8,64 @@
 
 import SwiftUI
 
+enum GameBoardViewType {
+    case currentState, diffHistory, sumHistory
+}
+
 struct GameBoardView: View {
+    @EnvironmentObject var settings: GameSettings
+    var activeView: GameBoardViewType { viewTypes[viewIndex % viewTypes.count]}
+    let viewTypes : [GameBoardViewType] =  [ .currentState, .diffHistory, .sumHistory ]
+    @State var viewIndex = 0
+    
+    @State var dragAmount: CGSize = .zero
+    @State var startingLocation: CGPoint?
+    @State var rotation: Double = 0.0
+    
+    var dragGesture : some Gesture {
+        DragGesture()
+        .onChanged() { value in
+            if let startLocation = startingLocation {
+                let distance = value.location.x - startLocation.x
+                dragAmount = CGSize(width: distance,
+                                    height: 0)
+                rotation = Double(distance / 5)
+            } else {
+                startingLocation = value.location
+            }
+        }
+        .onEnded() { value in
+            if dragAmount.width > 100.0 {
+                viewIndex = (viewIndex + 1) % viewTypes.count
+            } else if  dragAmount.width < -100.0 {
+                viewIndex = (viewIndex - 1 + viewTypes.count) % viewTypes.count
+            }
+            startingLocation = nil
+            dragAmount = .zero
+            rotation = 0.0
+        }
+    }
+    
+    var body: some View {
+        ZStack {
+            switch activeView {
+            case .currentState:
+                BoardUI(players: settings.players)
+            case .diffHistory:
+                ScoreTableView(history: settings.history, viewMode: .diff)
+            case .sumHistory:
+                ScoreTableView(history: settings.history, viewMode: .total)
+            }
+        }
+        .rotationEffect(Angle(degrees: rotation), anchor: .bottom)
+        .transition(.slide)
+        .offset(dragAmount)
+        .environmentObject(settings)
+        .gesture(dragGesture)
+    }
+}
+
+struct GameBoardView_Old: View {
     @EnvironmentObject var settings: GameSettings
     
     var body: some View {
