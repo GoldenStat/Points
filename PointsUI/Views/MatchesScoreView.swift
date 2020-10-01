346514e784f52ba61fe668baf88e63cdb0b03455
Alexander Voelz <alexandervoelz@icloud.com>
2020-10-01 10:04:53 +0200

    Match view is working

--- PointsUI/Views/MatchesScoreView.swift
+++ PointsUI/Views/MatchesScoreView.swift
@@ -9,37 +9,67 @@
 import SwiftUI
 
 struct MatchesScoreView: View {
-    var score: Score = Score(2, buffer: 2)
+    var score: Score = Score(0)
+    var body: some View {
+        MatchBox(score: score)
+    }
+}
+
+struct MatchBox: View {
+    
+    var score: Score
+    
+    let maxMatches = 5 // maximum matches is 5
+    
     var body: some View {
         ZStack {
             HStack {
-                ForEach (0 ..< score.value) { _ in
+                ForEach(1 ..< maxMatches) { count in
                     MatchView()
-                }
-                ForEach (0 ..< score.buffer) { _ in
-                    MatchView()
-                        .opacity(0.3)
-                }
-                ForEach (0 ..< 4 - score.sum) { _ in
-                    MatchView()
-                        .opacity(0.001)
+                        .opacity(opacity(for: score, matchNumber: count))
                 }
             }
-            MatchView()
-                .rotationEffect(.degrees(-70))
-                .scaleEffect(0.8)
+            
+            HStack {
+                ForEach(1 ..< maxMatches - 1) { count in
+                    MatchView()
+                        .opacity(0)
+                }
+                MatchView()
+                    .opacity(opacity(for: score, matchNumber: 5))
+                    .position()
+                    .rotationEffect(.degrees(-70))
+            }
         }
-        .frame(width: 400, height: 400)
+        .padding()
+        
+        Text("\(score.value) + \(score.buffer)")
+            .fontWeight(.bold)
+            .font(.system(size: 144))
+            .opacity(0.2)
+    }
+    
+    func opacity(for score: Score, matchNumber count: Int) -> Double {
+
+        let fullOpacity : Double = 1.0
+        let minOpacity : Double = 0.3
+
+        if score.value >= count {
+            return fullOpacity
+        } else if score.sum >= count {
+            return minOpacity
+        }
+        return 0
     }
 }
 
 let matchRatio : CGSize = CGSize(width: 0.2,
-                                 height: 0.8)
+                                 height: 1.0)
 
 struct MatchView: View {
     let bodyColor = Color.yellow
     let headColor = Color.red
-
+    
     var body: some View {
         ZStack {
             MatchBodyShape()
@@ -53,11 +83,11 @@ struct MatchView: View {
 struct MatchHeadShape: Shape {
     
     func path(in rect: CGRect) -> Path {
-
+        
         var path = Path()
-   
+        
         let centerX = rect.minX + rect.width / 2
-
+        
         let circleRadius : CGFloat = rect.width * matchRatio.width
         
         let circleCenter = CGPoint(x: centerX,
@@ -65,32 +95,33 @@ struct MatchHeadShape: Shape {
         
         let flatSide = Angle(degrees: 90)
         let flatWidthHalf = Angle(degrees: 30)
-
+        
         path.addArc(center: circleCenter, radius: circleRadius,
-               startAngle: flatSide - flatWidthHalf,
-               endAngle: flatSide + flatWidthHalf,
-               clockwise: true)
+                    startAngle: flatSide - flatWidthHalf,
+                    endAngle: flatSide + flatWidthHalf,
+                    clockwise: true)
         path.closeSubpath()
-
+        
         return path
     }
     
 }
 
 struct MatchBodyShape: Shape {
-        
+    
     func path(in rect: CGRect) -> Path {
-
+        
         var path = Path()
-   
+        
         let centerX = rect.minX + rect.width / 2
         let size = CGSize(width: rect.width * matchRatio.width,
-                          height: rect.height * matchRatio.height)
+                          height: rect.height // * matchRatio.height)
+        )
         
         let matchBodyRect = CGRect(x: centerX - size.width/2,
-                                   y: rect.minY + size.width * 1.8,
+                                   y: rect.minY, //+ size.width * 1.8,
                                    width: size.width,
-                                   height: size.height)
+                                   height: size.height) // * 0.8)
         
         path.addRect(matchBodyRect)
         
@@ -99,9 +130,33 @@ struct MatchBodyShape: Shape {
     
 }
 
+// MARK: - sample Views
+struct AnimatedMatchBox: View {
+    @State var score : Score = Score(0)
+    
+    var body: some View {
+        ZStack {
+            Color.background
+            MatchBox(score: score)
+        }
+        .onTapGesture {
+            withAnimation(.easeOut(duration: 1.0)) {
+                score.add(points: 1)
+            }
+            if score.buffer > 3 {
+                score.save()
+            }
+            if score.sum > 5 {
+                score = Score(0)
+            }
+        }
+    }
+}
+
 
 struct MatchesScoreView_Previews: PreviewProvider {
     static var previews: some View {
-        MatchesScoreView()
+        AnimatedMatchBox(score: Score(5))
+            .frame(width: 400, height: 400)
     }
 }
