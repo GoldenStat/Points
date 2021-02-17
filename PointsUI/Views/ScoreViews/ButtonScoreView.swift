754202087b43179391b7fe62824e19acedfeffd5
Alexander Voelz <alexandervoelz@icloud.com>
2021-02-17 14:38:15 +0100

    buffer view is flexible to height changes

--- PointsUI/Views/ScoreViews/ButtonScoreView.swift
+++ PointsUI/Views/ScoreViews/ButtonScoreView.swift
@@ -13,30 +13,34 @@ import SwiftUI
 struct ButtonScoreView: View {
     @EnvironmentObject var settings: GameSettings
     var score: Score
-        
+     
+    @State private var scaleFactor: CGFloat = 0.8
+    
+    // the text should be the biggest possible to look good, so we would get our available area, select a "box" out of it and calculate the best size
     var body: some View {
-        ZStack {
-            /// main View to fix space
-            Text("000")
-                .padding()
-                .foregroundColor(.clear)
-            Text(score.value.description)
+        GeometryReader { geo in
+            let size = min(geo.size.height,geo.size.width)
+                Text(score.value.description)
+                    .font(.system(size: size * scaleFactor, weight: .semibold, design: .rounded))
+                    .fixedSize(horizontal: true, vertical: false)
+                    .frame(width: size, height: size, alignment: .center)
         }
-        .font(.system(size: scoreSize, weight: .semibold, design: .rounded))
-        .fixedSize(horizontal: true, vertical: false)
-        .foregroundColor(.points)
-        .overlay(bufferView)
+            .scaledToFill()
+            .foregroundColor(.points)
+            .overlay(bufferView)
+            .background(Color.white.opacity(0.5))
     }
+    
+    let debugViewSize = true
+    var bgColor: Color { debugViewSize ? .white.opacity(0.5) : .clear }
   
-    private let scoreSize : CGFloat = 122
-
     @ViewBuilder var bufferView: some View {
         if score.buffer > 0 {
-            Text(score.buffer.description)
-                .font(bufferParam.font)
-                .fixedSize()
+                Text(score.buffer.description)
+                    .font(bufferParam.font)
+                    .fixedSize()
                 .foregroundColor(.blue)
-                .scaleEffect(bufferParam.scaleFactor)
+                .scaleEffect(bufferParam.scaleFactor * scaleFactor)
                 .onDrag() {
                     settings.cancelTimers() // timers  must be started in onDrop
                     settings.pointBuffer = score.buffer
@@ -67,37 +71,73 @@ struct ButtonScoreView: View {
         var offset: CGSize
         var font: Font = .system(size: 180.0, weight: .semibold, design: .rounded)
         static let appear = BufferParameters(scaleFactor: 0.3, offset: .zero)
-        static let final = BufferParameters(scaleFactor: 0.6, offset: CGSize(width: 40, height: -80))
+        static let final = BufferParameters(scaleFactor: 0.8, offset: CGSize(width: 80, height: -80))
     }
 }
 
 
 struct ButtonScorePreviewView: View {
     @State var settings = GameSettings()
-    @State var score = Score(11, buffer: 3)
+    @State var score = Score(1, buffer: 3)
+    @State var size : CGSize
+    
     var body: some View {
-        Color.background
-            .overlay(
+        VStack {
+            Color.background
+                .overlay(
+//                    LazyHGrid(rows: [GridItem(.flexible()),GridItem(.flexible())]) {
+                        LazyVGrid(columns: [GridItem(.flexible()),GridItem(.flexible())]) {
+                            ForEach(1..<4) { _ in
+                                button
+                                
+                            }
+                        }
+//                    }
+                )
+                .frame(width: size.width, height: size.height)
+                .environmentObject(settings)
+
+            HStack {
+                VStack {
+                    Slider(value: $size.height,
+                           in: height.min ... height.max,
+                           step: 5)
+                    label: do { Text("height") }
+                    Text(size.height.description)
+                }
+                VStack {
+                    Slider(value: $size.width,
+                           in: width.min ... width.max,
+                           step: 5)
+                    label: do { Text("width") }
+                    Text(size.width.description)
+                }
+            }
+        }
+    }
+    
+    let height = (min: CGFloat(60), max: CGFloat(600))
+    let width = (min: CGFloat(60), max: CGFloat(500))
+        
+    @ViewBuilder var button: some View {
         ButtonScoreView(score: score)
             .onTapGesture(count: 2) {
                 withAnimation {
-                score.save()
+                    score.save()
                 }
             }
             .simultaneousGesture(
                 TapGesture(count: 1)
                     .onEnded() {
                         withAnimation {
-                    score.add(points: 1)
+                            score.add(points: 1)
                         }
-                })
-        )
-            .environmentObject(settings)
+                    })
     }
 }
 struct ButtonScoreView_Previews: PreviewProvider {
     
     static var previews: some View {
-        ButtonScorePreviewView()
+        ButtonScorePreviewView(size: CGSize(width: 200, height: 400))
     }
 }
